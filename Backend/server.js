fba3838e23c9d03678cbfbd5573707f8cf7b3a94
haitrth <haithanhtran00@gmail.com>
2024-11-12 07:38:21 +0700

    Investing-psyology-app

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const affirmationsRoutes = require('./routes/affirmationsRoutes');
+const goalsRoutes = require('./routes/goalsRoutes');
+const reflectionsRoutes = require('./routes/reflectionsRoutes');
+const checklistRoutes = require('./routes/checklistRoutes');
+
+app.use(express.json());
+
+app.use('/api/affirmations', affirmationsRoutes);
+app.use('/api/goals', goalsRoutes);
+app.use('/api/reflections', reflectionsRoutes);
+app.use('/api/checklist', checklistRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
